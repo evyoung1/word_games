17e9604875cd9a1af4e063caf72ec04a5d94cd31
Eric Young <tyoung4@Erics-MacBook-Air-2.local>
2020-09-09 13:43:59 -0700

    initial code

---
+++ word_games.py
@@ -0,0 +1,11 @@
+def main(): 
+	with open('scrabble_words.txt', 'r') as f:
+        count=0
+		for line in f:
+            if count<5
+                print(line)
+                count=count+1
+            else
+                break
+if __name__ == '__main__':
+	main()
